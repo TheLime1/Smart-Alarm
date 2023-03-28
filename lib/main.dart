9297fd4646a082a8438c841dec2a646b17e86396
Aymen Hmani <47940043+TheLime1@users.noreply.github.com>
2023-03-28 18:07:00 +0100

    formatting & added try/catch
    try/catch for wrong characters (not numbers)

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-// creating text ediiting controller to take hour
-// and minute as input
+  // creating text ediiting controller to take hour
+  // and minute as input
   TextEditingController hourController = TextEditingController();
   TextEditingController minuteController = TextEditingController();
   @override
@@ -37,72 +37,83 @@ class _MyHomePageState extends State<MyHomePage> {
         centerTitle: true,
       ),
       body: Center(
-          child: Column(children: <Widget>[
-        SizedBox(height: 30),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 40,
-              width: 60,
-              decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  color: Colors.yellow,
-                  borderRadius: BorderRadius.circular(11)),
-              child: Center(
-                child: TextField(
-                  controller: hourController,
-                  keyboardType: TextInputType.number,
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 40,
+                  width: 60,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.circular(11)),
+                  child: Center(
+                    child: TextField(
+                      controller: hourController,
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
                 ),
+                SizedBox(width: 20),
+                Container(
+                  height: 40,
+                  width: 60,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.circular(11)),
+                  child: Center(
+                    child: TextField(
+                      controller: minuteController,
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.all(25),
+              child: TextButton(
+                child: const Text(
+                  'Create alarm',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                onPressed: () {
+                  int hour;
+                  int minutes;
+                  try {
+                    hour = int.parse(hourController.text);
+                    minutes = int.parse(minuteController.text);
+
+                    // creating alarm after converting hour
+                    // and minute into integer
+                    FlutterAlarmClock.createAlarm(hour, minutes);
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Invalid input'),
+                      ),
+                    );
+                  }
+                },
               ),
             ),
-            SizedBox(width: 20),
-            Container(
-              height: 40,
-              width: 60,
-              decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  color: Colors.yellow,
-                  borderRadius: BorderRadius.circular(11)),
-              child: Center(
-                child: TextField(
-                  controller: minuteController,
-                  keyboardType: TextInputType.number,
-                ),
+            ElevatedButton(
+              onPressed: () {
+                // show alarm
+                FlutterAlarmClock.showAlarms();
+              },
+              child: const Text(
+                'Show Alarms',
+                style: TextStyle(fontSize: 20.0),
               ),
-            ),
+            )
           ],
         ),
-        Container(
-          margin: const EdgeInsets.all(25),
-          child: TextButton(
-            child: const Text(
-              'Create alarm',
-              style: TextStyle(fontSize: 20.0),
-            ),
-            onPressed: () {
-              int hour;
-              int minutes;
-              hour = int.parse(hourController.text);
-              minutes = int.parse(minuteController.text);
-
-              // creating alarm after converting hour
-              // and minute into integer
-              FlutterAlarmClock.createAlarm(hour, minutes);
-            },
-          ),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            // show alarm
-            FlutterAlarmClock.showAlarms();
-          },
-          child: const Text(
-            'Show Alarms',
-            style: TextStyle(fontSize: 20.0),
-          ),
-        )
-      ])),
+      ),
     );
   }
 }
